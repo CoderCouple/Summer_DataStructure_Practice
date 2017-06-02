399324ad37341e8a9aa0f5600c74291a938a8a7b
Sunil Tiwari <sunil28071987@gmail.com>
2017-06-01 18:17:43 -0700

    Add files via upload

---
+++ coun7_Recursion.java
@@ -0,0 +1,24 @@
+
+public class coun7_Recursion {
+
+	public static int sumDigits(int n) {
+		  
+		  if(n == 0)
+		  return 0;
+		  
+		  int lastDigit = n%10;
+		  int remainingDigit = n/10;
+		  
+		  return lastDigit+sumDigits(remainingDigit);
+		  
+		}
+
+
+	public static void main(String[] args) {
+		int digitSum;
+		System.out.println("This is my first recursion Program..!!!");
+		digitSum = sumDigits(12345);
+		System.out.println("The sum of digits is :" + digitSum);
+	}
+
+}
